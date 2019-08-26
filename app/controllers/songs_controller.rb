53bec3bdc848706a9e1ed903c595aa9b921251e6
Travis Carter <carter.travis94@gmail.com>
2019-08-26 07:01:44 -0600

    Fix bugs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update, :destroy]
 
   def index
-    @songs = @billboard.songs.all
+    @songs = @billboard.songs
   end
 
   def show
